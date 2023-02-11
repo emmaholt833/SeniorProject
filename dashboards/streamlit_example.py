468ec64e7a0d03dbc42875e8874f575b25b66b10
Emma Holt <81181898+emmaholt833@users.noreply.github.com>
2023-02-11 11:46:47 -0700

    streamlit_update

--- dashboards/streamlit_example.py
+++ dashboards/streamlit_example.py
@@ -3,9 +3,21 @@ import pandas as pd
 
 ## streamlit tutorial here
 
-st.title("Welcome to Senior Project")
-st.write("some words")
+st.title("Welcome to my app")
+# st.write("some words")
 
-img = "data/ruby.jpg"
-st.image(img)
+# '''Magic words'''
+
+'''Look at my cute dog! :)'''
+
+img = "https://github.com/emmaholt833/SeniorProject/blob/main/dashboards/data/ruby.jpg?raw=true"
+
+# pic = "C:/Users/Emma/Documents/WI23/SeniorProject/SeniorProject_github/dashboards/data/ruby.jpg"
+# pic2 = "/data/ruby.jpg"
+st.image(img, width = 700)
+
+name = st.text_input(label = "What is your name?")
+
+st.write("Hello " + name + " how are you today?")
+
 
